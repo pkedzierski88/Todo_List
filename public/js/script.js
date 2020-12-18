1d07a0d09387fc7d64365e774fea299c2d9d0790
Piotr Kedzierski <piotr.kedzierski.88@gmail.com>
2020-12-18 13:18:33 +0100

    naprawa bledow przy dodawaniu zadan

--- public/js/script.js
+++ public/js/script.js
@@ -7,40 +7,33 @@
     }
 }(jQuery));
 
-$("input[type='text']").keypress(function(event){
-    if(event.which === 13){
-        if($(this).val() < 1) {
-            console.log("Empty String!");
-        } else{
-            $("#inputForm").submit(function (e) {
-                e.preventDefault();
-                let toDoItem = $(this).serialize();
-                $.post("/todos", toDoItem, function(data) {
-                    $("#toDoList").append(
-                        `
-                        <li>
-                            <form class="editForm">
-                                <span>Edytuj: </span><input type=text name=todo[edit] value="${data.text}" placeholder="Edytuj..." maxlength="30" autocomplete="off" required>
-                                <button>Zapisz</button>
-                            </form>
-                            <form class="checkForm" action="/todos/${data._id}" method="POST">
-                                <button class="checkButton">
-                                    <i class="far fa-circle"></i>
-                                </button>
-                            </form>
-                            <span>${data.text}</span>
-                            <form class="deleteForm" action="/todos/${data._id}" method="POST">
-                                <button class="deleteButton"><i class="fas fa-trash-alt"></i></button>
-                            </form>
-                            <button class="editButton"><i class="fas fa-pencil-alt"></i></button>
-                        </li>
-                        `
-                    );
-                    $("#inputForm").find("input").val("");
-                });
-            });
-        }
-    }
+
+$("#inputForm").submit(function (e) {
+    e.preventDefault();
+    let toDoItem = $(this).serialize();
+    $.post("/todos", toDoItem, function(data) {
+        $("#toDoList").append(
+            `
+            <li>
+                <form class="editForm">
+                    <span>Edytuj: </span><input type=text name=todo[edit] value="${data.text}" placeholder="Edytuj..." maxlength="30" autocomplete="off" required>
+                    <button>Zapisz</button>
+                </form>
+                <form class="checkForm" action="/todos/${data._id}" method="POST">
+                    <button class="checkButton">
+                        <i class="far fa-circle"></i>
+                    </button>
+                </form>
+                <span>${data.text}</span>
+                <form class="deleteForm" action="/todos/${data._id}" method="POST">
+                    <button class="deleteButton"><i class="fas fa-trash-alt"></i></button>
+                </form>
+                <button class="editButton"><i class="fas fa-pencil-alt"></i></button>
+            </li>
+            `
+        );
+        $("#inputForm").find("input").val("");
+    });
 });
 
 $("#toDoList").on("click", ".editButton", function(){
